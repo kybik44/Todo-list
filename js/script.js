8dbc792bdcca521b97bd506166b4673330d3fd27
kybik44 <kybikfeed@gmail.com>
2021-06-21 23:30:28 +0300

    get Edit Task

--- js/script.js
+++ js/script.js
@@ -1,29 +1,39 @@
 let $modal = document.querySelector(".modal");
 let $fields = document.querySelectorAll(".popup-field");
 let $table = document.querySelector(".table");
-
+let currTaget;
 document.addEventListener("click", function (event) {
 
     const target = event.target;
     let $parentTr = target.closest("tr");
-    if (target.classList.contains('modal') || target.classList.contains('close')) {
-        toggleClass('close', $modal);
-    }
+    if (target.classList.contains('modal') || target.classList.contains('close') ) toggleClass('close', $modal);
     if (target.classList.contains('ok')) {
         renderTable(addNewTask());
         toggleClass('close', $modal);
     }
+    if (target.classList.contains('edit')) {
+        renderTable(JSON.parse(localStorage.getItem("mainData")));
+        toggleClass('close', $modal);
+    }
     if (target.classList.contains('add-task')) toggleClass('open', $modal);
-    if (target.classList.contains("btn-delete")) CRUD($parentTr, addNewTask(), "dataDel", "mainData");
-    if (target.classList.contains("btn-delete-gg")) CRUD($parentTr, JSON.parse(localStorage.getItem("dataFin")), undefined, "dataFin");
-    if (target.classList.contains("btn-finish")) CRUD($parentTr, addNewTask(), "dataFin", "mainData");
-    if (target.classList.contains("btn-restore")) CRUD($parentTr, JSON.parse(localStorage.getItem("dataDel")), "mainData", "dataDel");
+    if (target.classList.contains("btn-delete")) CD($parentTr, addNewTask(), "dataDel", "mainData");
+    if (target.classList.contains("btn-delete-fin")) CD($parentTr, JSON.parse(localStorage.getItem("dataFin")), undefined, "dataFin");
+    if (target.classList.contains("btn-delete-del")) CD($parentTr, JSON.parse(localStorage.getItem("dataDel")), undefined, "dataDel");
+    if (target.classList.contains("btn-finish")) CD($parentTr, addNewTask(), "dataFin", "mainData");
+    if (target.classList.contains("btn-restore")) CD($parentTr, JSON.parse(localStorage.getItem("dataDel")), "mainData", "dataDel");
+    if (target.classList.contains("btn-edit")) {
+        toggleClass('open', $modal);
+        currTaget = target;
+    };
+    if(target.classList.contains("edit")){
+        editTask(currTaget.closest("tr"), addNewTask());
+    }
     if (target.classList.contains("menu__link")) renderTable(target.getAttribute('href'));
 
 });
 
 renderTable("#main");
-function toggleClass(evt, versModal) {
+function toggleClass(evt, versModal, index) {
     if (evt === "open") versModal.classList.add("active");
     if (evt === "close") {
         versModal.classList.remove("active");
@@ -37,6 +47,9 @@ function validate() {
             inpt.value = '';
         }
     }
+    function emptyFields(){
+        
+    }
     return {
         clearForm
     }
@@ -45,7 +58,7 @@ function validate() {
 
 function addNewTask() {
     let mainData = JSON.parse(localStorage.getItem("mainData")) || [];
-    let newTask = [...$fields].reduce((obj, item) => {
+    let newTask  = [...$fields].reduce((obj, item) => {
         obj[item.name] = item.value;
         return obj;
     }, {});
@@ -55,24 +68,32 @@ function addNewTask() {
     localStorage.setItem("mainData", JSON.stringify(mainData));
     return mainData;
 }
-
-function CRUD(targetTask, mainData, currData, dataName) {
-    let $colTd = targetTask.children;
-    for (let el of $colTd) {
-        for (let index in mainData) {
-            if (el.textContent === mainData[index].id) {
-                console.log(mainData, currData, dataName)
-                let currentData = JSON.parse(localStorage.getItem(currData)) || [];
-                if (currData) currentData.push(mainData[index]);
-                mainData.splice(index, 1);
-                localStorage.setItem(currData, JSON.stringify(currentData));
-                targetTask.innerHTML = "";
-                localStorage.setItem(dataName, JSON.stringify(mainData));
-            }
+function CD(target, mainData, currData, dataName) {
+        mainData.forEach((element, index) => {
+        if (+target.dataset.id === index) {
+            let currentData = JSON.parse(localStorage.getItem(currData)) || [];
+            if (currData) currentData.push(element);
+            mainData.splice(index, 1);
+            localStorage.setItem(currData, JSON.stringify(currentData));
+            target.innerHTML = "";
+            localStorage.setItem(dataName, JSON.stringify(mainData));
         }
-    }
+    });
 }
-
+function editTask(targetTr, mainData){
+    console.log(targetTr);
+    for(let key in mainData){
+        if (targetTr.dataset.id === key) {
+            console.log(mainData[key])
+            mainData[key] = [...$fields].reduce((obj, item) => {
+                console.log(item.value)
+                obj[item.name] = item.value;
+                return obj;
+            }, {}); 
+        }
+    };
+    // localStorage.setItem("mainData", JSON.stringify(mainData));
+}
 
 function renderTable(href) {
     let $mainTitle = document.querySelector(".main__title");
@@ -116,10 +137,9 @@ function renderTable(href) {
     }
     $mainTitle.innerHTML = getTitle(href);
     let data = JSON.parse(localStorage.getItem(getData(href))) || [];
-
-   
-    $table.innerHTML = data.reduce((str, item) => {
-        str += `<tr>    <td>${ item.id}</td>
+    console.log(data)
+    $table.innerHTML = data.reduce((str, item, index) => {
+        str += `<tr data-id="${index}">    
                         <td>${ item.taskName }</td>
                         <td>${ item.taskDesc }</td>
                         <td>${ getPriority( +item.priority ) }</td>
@@ -128,7 +148,8 @@ function renderTable(href) {
                         ${href === "#main" ? `<button class="btn-edit">Редактировать</button>` : ""}
                         ${href === "#main" ? `<button class="btn-finish">Выполнить</button>` : ""}
                         ${href === "#deleted" ? `<button class="btn-restore">Восстановить</button>` : ""}
-                        ${href === "#done" ? `<button class="btn-delete-gg">Удалить полностью</button>` : ""}
+                        ${href === "#deleted" ? `<button class="btn-delete-del">Удалить полностью</button>` : ""}
+                        ${href === "#done" ? `<button class="btn-delete-fin">Удалить полностью</button>` : ""}
                         ${href === "#done" ? `<button class="btn-edit">Редактировать</button>` : ""}
                         </td>
                       </tr>`;
